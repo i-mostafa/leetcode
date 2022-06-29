e342fc455c714dad9180fdab3440b64b8c465454
Islam Mostafa <islam.mostafa.fahmey@gmail.com>
2022-06-30 00:01:28 +0200

    solution for #15

---
+++ 15-3sum.js
@@ -0,0 +1,45 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number[][]}
+//  */
+// var threeSum = function (nums) {
+//   nums = nums.sort((a, b) => a - b);
+//   const result = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     if (i != 0 && nums[i] === nums[i - 1]) continue;
+//     const holder = {};
+//     for (let j = i + 1; j < nums.length; j++) {
+//       const complement = -(nums[i] + nums[j]);
+//       if (complement in holder) {
+//         result.push([nums[i], complement, nums[j]]);
+//         while (j < nums.length - 1 && nums[j] === nums[j + 1]) j++;
+//       }
+//       holder[nums[j]] = true;
+//     }
+//   }
+//   return result;
+// };
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+  nums = nums.sort((a, b) => a - b);
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let j = i + 1;
+    let k = nums.length - 1;
+    while (j < k) {
+      if (nums[i] + nums[j] + nums[k] === 0) {
+        console.log(nums[i], nums[j], nums[k]);
+        result.push([nums[i], nums[j], nums[k]]);
+        j++;
+        while (j < nums.length - 1 && nums[j] === nums[j - 1]) j++;
+      } else if (nums[i] + nums[j] + nums[k] < 0) j++;
+      else k--;
+    }
+  }
+  return result;
+};
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
